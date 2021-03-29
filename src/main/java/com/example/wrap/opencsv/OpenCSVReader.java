227b7f5d9f0399676ef78ba6ad3b6e2fd53e465d
blocking <18510855440@sina.cn>
2021-03-29 22:24:46 +0800

    add temp csv paser

--- src/main/java/com/example/wrap/opencsv/OpenCSVReader.java
+++ src/main/java/com/example/wrap/opencsv/OpenCSVReader.java
@@ -1,19 +1,22 @@
 package com.example.wrap.opencsv;
 
+import com.beust.jcommander.internal.Lists;
+import com.beust.jcommander.internal.Sets;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderHeaderAware;
+import com.opencsv.CSVWriter;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.net.URL;
+import java.io.*;
+import java.net.URLDecoder;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  *http://opencsv.sourceforge.net/
@@ -22,7 +25,57 @@ import java.util.Map;
  */
 public class OpenCSVReader {
     private static final String SAMPLE_CSV_FILE_PATH = "D:\\test\\csv\\users.csv";
+    private static final String TEMP_SAMPLE_CSV_FILE_PATH = "D:\\test\\csv\\dsapi.xuban.v1-data.csv";
+    private static final String TEMP_SAMPLE_CSV_FILE_PATH_OUT = "D:\\test\\csv\\dsapi.xuban.v1-data-out.csv";
     private static final String STRING_ARRAY_SAMPLE = "./string-array-sample.csv";
+
+
+    @Test
+    public  void sample2() throws Exception {
+        Reader reader = Files.newBufferedReader(Paths.get(TEMP_SAMPLE_CSV_FILE_PATH));
+        CSVReader csvReader = new CSVReader(reader);
+        String[] nextRecords;
+        Set<Temp> temps = Sets.newLinkedHashSet();
+        while((nextRecords = csvReader.readNext()) != null){
+            String nextRecord = nextRecords[9];
+            String[] split = nextRecord.split("&");
+            Temp temp = new Temp();
+            Arrays.stream(split).forEach(str->{
+                int idx = str.indexOf("=");
+                if(idx == -1){
+                    return;
+                }
+                try {
+                    String key = URLDecoder.decode(str.substring(0, idx), "UTF-8");
+                    String value = URLDecoder.decode(str.substring(idx + 1), "UTF-8");
+                    if("studentCode".equals(key)){
+                        temp.setStudentCode(value);
+                    }
+                    if("schoolId".equals(key)){
+                        temp.setSchoolId(value);
+                    }
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            });
+            temps.add(temp);
+            System.out.println("args : " + nextRecord+"::::"+temp);
+
+        }
+
+
+        try (
+                Writer writer = Files.newBufferedWriter(Paths.get(TEMP_SAMPLE_CSV_FILE_PATH_OUT));
+        ) {
+            StatefulBeanToCsv<Temp> beanToCsv = new StatefulBeanToCsvBuilder(writer)
+                    .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
+                    .build();
+
+            beanToCsv.write(Lists.newArrayList(temps));
+        }
+
+    }
+
 
     @Test
     public  void sample() throws IOException {
---
+++ src/main/java/com/example/wrap/opencsv/Temp.java
@@ -0,0 +1,9 @@
+package com.example.wrap.opencsv;
+
+import lombok.Data;
+
+@Data
+public class Temp {
+    private String studentCode;
+    private String schoolId;
+}
